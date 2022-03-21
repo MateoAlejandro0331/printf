0538703f84ac767f8100c58be88c6376e325f7a8
jhojanperlaza <jhojanperlaza@gmail.com>
2022-03-21 17:09:13 -0500

    header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H	
+/*
+* File: main.h
+* Auth: Mateo López - Jhojan Perlaza
+* Desc: Header file containing declarations for all functions
+* used in the _printf.
+*/
+
+int _printf(const char *format, ...);
+
+#endif
